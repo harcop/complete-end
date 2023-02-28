ef32a7caa972e93de7fad03016de34413c33d776
harcop <harcopdrew@gmail.com>
2023-03-01 00:24:10 +0100

    refactor test

--- test.ts
+++ test.ts
@@ -1,37 +1,44 @@
 import test from 'ava';
 import completeString from './index';
 
-test('complete a string with desire ending', t => {
-  const url = 'https://url.com'
+test('should append the desired ending if it is not already present', t => {
+  const inputString = 'https://url.com'
   const end = '/api/v4'
   const expected = 'https://url.com/api/v4'
-  t.is(completeString(url, end), expected)
+  t.is(completeString(inputString, end), expected)
 })
 
-test('complete a string with partial end', t => {
-  const url = 'https://url.com/api'
+test('should append the remaining characters of the desired ending if it partially matches the end of the input string', t => {
+  const inputString = 'https://url.com/api'
   const end = '/api/v4'
   const expected = 'https://url.com/api/v4'
-  t.is(completeString(url, end), expected)
+  t.is(completeString(inputString, end), expected)
 })
 
-test('complete a string with a missing end', t => {
-  const url = 'https://url.com/api/v'
+test('should not modify the input string if the desired ending is already present', t => {
+  const inputString = 'https://url.com/api/v4'
   const end = '/api/v4'
   const expected = 'https://url.com/api/v4'
-  t.is(completeString(url, end), expected)
+  t.is(completeString(inputString, end), expected)
 })
 
-test('do nothing when the string has the same ending', t => {
-  const url = 'https://url.com/api/v4'
-  const end = '/api/v4'
-  const expected = 'https://url.com/api/v4'
-  t.is(completeString(url, end), expected)
-})
-
-test('complete a string with a lengthy end', t => {
-  const url = 'https://url.com/api/v4'
+test('should handle edge cases where the desired ending is longer than the input string', t => {
+  const inputString = 'https://url.com/api/v4'
   const end = '/api/v4/another-endpoint-url'
   const expected = 'https://url.com/api/v4/another-endpoint-url'
-  t.is(completeString(url, end), expected)
+  t.is(completeString(inputString, end), expected)
 })
+
+test("should handle edge cases where the desired ending is an empty string", (t) => {
+  const inputString = "Hello world";
+  const desiredEnding = "";
+
+  t.is(completeString(inputString, desiredEnding), "Hello world");
+});
+
+test("should handle edge cases where the input string is an empty string", (t) => {
+  const inputString = "";
+  const desiredEnding = "Hello";
+
+  t.is(completeString(inputString, desiredEnding), "Hello");
+});
